00feaf605f96a0fa513037b33c8bc98ce78b1659
Vitalii Fedorets <vsfedoretc93@gmail.com>
2024-05-02 17:59:26 +0200

    Class Change CaesarCipher added method public static String decrypt(String encryptedText)

--- src/main/java/ua/javarush/fedorets/cryptoanalyzer/CaesarCipher/CaesarCipher.java
+++ src/main/java/ua/javarush/fedorets/cryptoanalyzer/CaesarCipher/CaesarCipher.java
@@ -10,25 +10,47 @@ public class CaesarCipher {
 
         text = text.toLowerCase();
 
-
         for (char symbol : text.toCharArray()) {
 
             int index = indexOf(symbol);
 
             if (index != -1) {
+
                 int newIndex = (index + SHIFT) % CaesarAlphabet.CaeserAlphabet.length;
                 encryptedText.append(CaesarAlphabet.CaeserAlphabet[newIndex]);
             } else {
-
                 encryptedText.append(symbol);
             }
         }
-
         return encryptedText.toString();
     }
 
+
+    public static String decrypt(String encryptedText) {
+
+        StringBuilder decryptedText = new StringBuilder();
+
+        for (char symbol : encryptedText.toCharArray()) {
+
+            int index = indexOf(symbol);
+
+            if (index != -1) {
+
+                int newIndex = (index - SHIFT + CaesarAlphabet.CaeserAlphabet.length) % CaesarAlphabet.CaeserAlphabet.length;
+                decryptedText.append(CaesarAlphabet.CaeserAlphabet[newIndex]);
+            } else {
+
+                decryptedText.append(symbol);
+            }
+        }
+
+        return decryptedText.toString();
+    }
+
     private static int indexOf(char symbol) {
+
         for (int i = 0; i < CaesarAlphabet.CaeserAlphabet.length; i++) {
+
             if (CaesarAlphabet.CaeserAlphabet[i] == symbol) {
                 return i;
             }
